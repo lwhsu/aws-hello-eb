b91a0ad6d4e79bc9d61bdcbcedc9accd57923043
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-05-31 19:47:01 +0800

    CircleCI

---
+++ create-app.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://dl.bintray.com/lwhsu/maven/org/lwhsu/test/hello-ws/1.8/hello-ws-1.8.war
+mv hello-ws-1.8.war hello-ws.war
+
+zip app.zip Dockerfile Dockerrun.aws.json hello-ws.war
